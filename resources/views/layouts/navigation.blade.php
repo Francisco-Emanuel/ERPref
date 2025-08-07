1cb36b7f23bf37f72f467226948a768707c8daa5
ChicoTI <chico_emanuelsoares@hotmail.com>
2025-08-07 14:00:02 -0300

    a

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
             <div class="flex">
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-[#1d4ed8]" />
                     </a>
                 </div>
 
